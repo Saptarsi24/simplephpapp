98570f78c4c4cc59318fd0452bb5f7c4800428b0
Pawan Bharat Mhaske <109577088+Pawan-1008@users.noreply.github.com>
2024-09-02 12:38:47 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First PHP pages</title>
+</head>
+<body>
+
+    <?php
+        echo "<h1>Hello Linkedin Family! Application V1.0</h1>";
+    ?>
+
+</body>
+</html>
